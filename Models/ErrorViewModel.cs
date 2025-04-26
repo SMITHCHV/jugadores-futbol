d61135c80e6e110dfc9288721ef1fcab1168ffb3
Smith <waltersmithchv@gmail.com>
2025-04-25 23:04:33 -0500

    Inicial: Proyecto MVC base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace jugadores_futbol.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
